a985bbe32c01fb91af92cd76434c4ef56ddf2704
Michael Nguyen <mikeyng93@gmail.com>
2023-07-11 10:13:02 -0700

    Login Functionality Added

--- base/views.py
+++ base/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 from .models import Thread, Section
 from .forms import ThreadForm
 
@@ -11,6 +14,27 @@ threads = [
     {'id':3, 'name':'Frontend Help'},
 
 ]
+
+def loginPage(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or Password is incorrect')
+
+    context = {}
+    return render(request, 'base/login_register.html', context)
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
@@ -63,3 +87,4 @@ def deleteThread(request, pk):
         return redirect('home')
 
     return render(request, 'base/delete.html', {'obj':thread})
+    
